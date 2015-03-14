fe4f8faa943fa9ec070b4545bfde05577c884715
Vishal <vishivish18@gmail.com>
2015-03-15 03:35:02 +0530

    Changed manufacturer to Company

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -1,20 +1,20 @@
 /**
- * ManufacturerController
+ * CompanyController
  *
- * @description :: Server-side logic for managing manufacturers
+ * @description :: Server-side logic for managing Companys
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 module.exports = {
-	 'add-manufacturer': function(req,res){
+	 'add-Company': function(req,res){
    
         res.view();
     },
     
     
-      addmanufacturer: function(req,res,next){
+      addCompany: function(req,res,next){
 
-                        Manufacturer.create({ mcode: req.param('mcode'), mname: req.param('mname'), maddress: req.param('maddress'), mopeningbalance: req.param('mopeningbalance'), mbstcode: req.param('mbstcode') }, function userCreated(err,manufacturer) {
+                        Company.create({ mcode: req.param('mcode'), mname: req.param('mname'), maddress: req.param('maddress'), mopeningbalance: req.param('mopeningbalance'), mbstcode: req.param('mbstcode') }, function userCreated(err,Company) {
 
                                         if (err) {
                                             console.log(err);
@@ -23,19 +23,19 @@ module.exports = {
                                             }
 
                                         }
-                                       // res.json(manufacturer);
-                                res.redirect('/manufacturer');
+                                       // res.json(Company);
+                                res.redirect('/Company');
 
                         });
         },
     
     
      index: function (req, res, next){
-        Manufacturer.find(function foundManufacturers (err, manufacturer){
+        Company.find(function foundCompanys (err, Company){
             if(err) return next (err);
            
             res.view({
-            manufacturer: manufacturer
+            Company: Company
             });
         
         });
